e71c5bd7593c5aac40032e042c41699dd10e6f76
Numan <numanbtt@gmail.com>
2023-02-11 18:28:16 +0500

    update activity option added
    update activity option added

--- frontEnd/src/components/dashboard/AddNewActivityCard.jsx
+++ frontEnd/src/components/dashboard/AddNewActivityCard.jsx
@@ -22,14 +22,14 @@ const AddNewActivityCard = () => {
 		setActivityOptions(response);
 	};
 
-	// to set default value in date input
-	const [date, setDate] = useState(new Date().toISOString().substr(0, 10));
 	// Get data from the inputs
 	const [handle, setHandle] = useState({
 		activity: "Aerobics",
 		startTime: "12:00",
 		duration: 30,
 		date: new Date().toISOString().substr(0, 10),
+		// date: new Date().toLocaleString().substr(0, 10),
+		// date: new Date().toDateString(),
 	});
 	const handler = (e) => {
 		setHandle({ ...handle, [e.target.name]: e.target.value });
@@ -47,98 +47,110 @@ const AddNewActivityCard = () => {
 	};
 
 	// Get data for update condition and save it in state
-	const [updateHandle, setUpdateHandle] = useState({});
 	const getUpdateActivity = async () => {
 		var data = await fetch(
 			`http://127.0.0.1:4000/useractivities/activity/${userActivityUpdateID}`
 		);
 		data = await data.json();
-		console.log(data);
-		setUpdateHandle(data);
+		handle.activity = data.activity;
+		handle.startTime = data.startTime;
+		handle.duration = data.duration;
+		let dateString = new Date(data.date);
+		handle.date = dateString.toISOString().substr(0, 10);
 	};
 
 	// Update Activity in Database
-	const updateActivity = async () => {};
+	const updateActivity = async () => {
+		console.log(handle);
+		await fetch(
+			`http://127.0.0.1:4000/useractivities/${userActivityUpdateID}`,
+			{
+				method: "PUT",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(handle),
+			}
+		);
+	};
 
 	useEffect(() => {
-		console.log(isUpdating);
+		// console.log(isUpdating);
 		getUpdateActivity();
 		fetchActivities();
-	}, [isOpen, isUpdating]);
+	}, [isOpen]);
 
 	return (
 		<>
-			{isOpen && (
-				<div>
-					<div className="h-full">
-						<div className="fixed top-0 left-0 bottom-0 right-0 h-screen glassmorphism bg-opacity-40">
-							<div className="h-full  flex flex-row justify-center items-center fixed top-0 left-0 bottom-0 right-0">
-								<div className="min-h-[80vh] w-[30vw] bg-white shadow-2xl rounded-lg">
+			{/* {isOpen && ( */}
+			<div>
+				<div className="h-full">
+					<div className="fixed top-0 left-0 bottom-0 right-0 h-screen glassmorphism bg-opacity-40">
+						<div className="h-full  flex flex-row justify-center items-center fixed top-0 left-0 bottom-0 right-0">
+							<div className="min-h-[80vh] w-[30vw] bg-white shadow-2xl rounded-lg">
+								<div>
+									<img
+										src="../../mma.jpg"
+										alt=""
+										className="object-cover object-center rounded-t-lg"
+									/>
+								</div>
+								<div className="py-2 px-5 flex justify-between items-center">
+									<h1 className="font-semibold">Select Activity</h1>
+									<select
+										className="w-48 rounded-sm"
+										onChange={handler}
+										value={handle.activity}
+										name="activity"
+									>
+										{activityOptions.map((element) => (
+											<option
+												className="bg-black hover:bg-blue-700 p-1 glassmorphism bg-opacity-75 rounded-md text-white"
+												key={element._id}
+												value={element.activity}
+											>
+												{element.activity}
+											</option>
+										))}
+									</select>
+								</div>
+								<hr />
+								<div className="py-2 px-5 flex justify-between items-center">
+									<h1 className="font-semibold">Start Time</h1>
+									<input
+										type="time"
+										className="w-48 rounded-sm"
+										placeholder="minutes"
+										onChange={handler}
+										name="startTime"
+										value={handle.startTime}
+									/>
+								</div>
+								<div className="py-2 px-5 flex justify-between items-center">
 									<div>
-										<img
-											src="../../mma.jpg"
-											alt=""
-											className="object-cover object-center rounded-t-lg"
-										/>
+										<h1 className="font-semibold text-center">Duration</h1>
+										<h1 className="text-sm text-center">(minutes)</h1>
 									</div>
-									<div className="py-2 px-5 flex justify-between items-center">
-										<h1 className="font-semibold">Select Activity</h1>
-										<select
-											className="w-48 rounded-sm"
-											onChange={handler}
-											value={handle.activity}
-											name="activity"
-										>
-											{activityOptions.map((element) => (
-												<option
-													className="bg-black hover:bg-blue-700 p-1 glassmorphism bg-opacity-75 rounded-md text-white"
-													key={element._id}
-													value={element.activity}
-												>
-													{element.activity}
-												</option>
-											))}
-										</select>
-									</div>
-									<hr />
-									<div className="py-2 px-5 flex justify-between items-center">
-										<h1 className="font-semibold">Start Time</h1>
-										<input
-											type="time"
-											className="w-48 rounded-sm"
-											placeholder="minutes"
-											onChange={handler}
-											name="startTime"
-											value={handle.startTime}
-										/>
-									</div>
-									<div className="py-2 px-5 flex justify-between items-center">
-										<div>
-											<h1 className="font-semibold text-center">Duration</h1>
-											<h1 className="text-sm text-center">(minutes)</h1>
-										</div>
-										<input
-											type="number"
-											className="w-48 rounded-sm"
-											placeholder="minutes"
-											onChange={handler}
-											name="duration"
-											value={handle.duration}
-										/>
-									</div>
-									<hr />
-									<div className="py-2 px-5 flex justify-between items-center">
-										<h1 className="font-semibold">Date</h1>
-										<input
-											type="date"
-											className="w-48 rounded-sm"
-											placeholder="minutes"
-											onChange={handler}
-											name="date"
-											value={handle.date}
-										/>
-									</div>
-
+									<input
+										type="number"
+										className="w-48 rounded-sm"
+										placeholder="minutes"
+										onChange={handler}
+										name="duration"
+										value={handle.duration}
+									/>
+								</div>
+								<hr />
+								<div className="py-2 px-5 flex justify-between items-center">
+									<h1 className="font-semibold">Date</h1>
+									<input
+										type="date"
+										className="w-48 rounded-sm"
+										placeholder="minutes"
+										onChange={handler}
+										name="date"
+										value={handle.date}
+									/>
+								</div>
+								{isOpen ? (
 									<div className="flex justify-center items-center space-x-5 my-2 mb-4">
 										<button
 											className="rounded-md text-lg py-2 px-5 text-center hover:scale-105 transition duration-300 border-2 border-black bg-black text-white"
@@ -159,14 +171,59 @@ const AddNewActivityCard = () => {
 											Cancel
 										</button>
 									</div>
-								</div>
+								) : (
+									<div className="flex justify-center items-center space-x-5 my-2 mb-4">
+										<button
+											className="rounded-md text-lg py-2 px-5 text-center hover:scale-105 transition duration-300 border-2 border-black bg-black text-white"
+											onClick={() => {
+												handle.date === "" || handle.startTime === ""
+													? alert("Enter Complete Data")
+													: updateActivity();
+												dispatch(setUpdateState());
+												console.log("clicked");
+											}}
+										>
+											Update Activity
+										</button>
+										<button
+											className="rounded-md text-lg py-2 px-5 text-center hover:scale-105 transition duration-300 border-black text-black border-2"
+											onClick={() => {
+												// dispatch(addActivityState());
+												dispatch(setUpdateState());
+											}}
+										>
+											Cancel
+										</button>
+									</div>
+								)}
+								{/* <div className="flex justify-center items-center space-x-5 my-2 mb-4">
+										<button
+											className="rounded-md text-lg py-2 px-5 text-center hover:scale-105 transition duration-300 border-2 border-black bg-black text-white"
+											onClick={() => {
+												handle.date === "" || handle.startTime === ""
+													? alert("Enter Complete Data")
+													: saveActivity();
+											}}
+										>
+											Add Activity
+										</button>
+										<button
+											className="rounded-md text-lg py-2 px-5 text-center hover:scale-105 transition duration-300 border-black text-black border-2"
+											onClick={() => {
+												dispatch(addActivityState());
+											}}
+										>
+											Cancel
+										</button>
+									</div> */}
 							</div>
 						</div>
 					</div>
 				</div>
-			)}
+			</div>
+			{/* )} */}
 			{/* To Update Data */}
-			{isUpdating && (
+			{/* {isUpdating && (
 				<div>
 					<div className="h-full">
 						<div className="fixed top-0 left-0 bottom-0 right-0 h-screen glassmorphism bg-opacity-40">
@@ -263,7 +320,7 @@ const AddNewActivityCard = () => {
 						</div>
 					</div>
 				</div>
-			)}
+			)} */}
 		</>
 	);
 };
